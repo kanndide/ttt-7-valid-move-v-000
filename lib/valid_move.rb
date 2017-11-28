ff6ab694a5395b52425cec16503679b7bdabf76e
William Kann <william.l.kann@gmail.com>
2017-11-28 16:00:22 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ def valid_move?(board, index)
     true
   elsif position_taken?(board, index) == true
     false
-  elsif position_taken(board, index) != int.between(0,8)
+  elsif position_taken(board, index) != index.between(0,8)
   end
 end
 
