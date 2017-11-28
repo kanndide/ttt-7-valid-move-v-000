1232a0836de43d2d13694baef2b44330f7d248c5
William Kann <william.l.kann@gmail.com>
2017-11-28 16:48:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,7 @@ def valid_move?(board, index)
   elsif position_taken?(board, index) == true
     false
   end
-  if board[index].between?(0, 8) == false
+  if board[index].between?(1, 9) == false
     false
   end
 end
