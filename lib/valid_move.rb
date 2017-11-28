40d09e74148bfb0fcb4668ad9ddce8cab3000819
William Kann <william.l.kann@gmail.com>
2017-11-28 15:44:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 require_relative "../lib/position_taken.rb"
 
-def valid_move? (position_taken)
+def valid_move? (position_taken?)
   if position_taken == false
     true
   elsif postion_taken == true
@@ -9,7 +9,7 @@ def valid_move? (position_taken)
   end
 end
 
-
+position_taken?(15)
 valid_move?
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
